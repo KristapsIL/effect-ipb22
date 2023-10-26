29307dfa677a1a63fd075c651086c7043e4d07c3
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-10-26 09:52:43 +0300

    aaa

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Todo from "./Todo";
 function App() {
+  const todos = [{
+    userId: 1,
+    id:1,
+    title:"delectus",
+    completed: false,
+  }];
+  const todoJSX = todos.map((doom, index)=>{
+    <Todo/>
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Hi</h1>
+    <Todo/>
+    </>
   );
 }
 
---
+++ src/Todo.js
@@ -0,0 +1,10 @@
+import {useState} from "react";
+function Todo(props){
+    return(
+    <>
+    <h2>Todo</h2>
+    <input type="checkbox" />
+    </>
+    )
+}
+export default Todo;
