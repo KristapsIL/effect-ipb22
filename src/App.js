a57022062e579df32f19ed05b0f79a042b04c1d3
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-10-31 09:53:45 +0200

    what is this

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import {useState, useEffect} from "react";
+
 import Todo from "./Todo";
+
 function App() {
-  const toDo = {
-    userId: 1,
-    id:1,
-    title:"delectus",
-    completed: true,
-  };
+  const [toDo, setTodo] = useState({});
+  useEffect(()=>{
+    async function getTodo(){
+      const response = await fetch("https://jsonplaceholder.typicode.com/todos/10");
+      const data = await response.json();
+      setTodo(data);
+    }
+    getTodo();
+  },[])
+
+  // const toDo = {
+  //   userId: 1,
+  //   id:1,
+  //   title:"delectus",
+  //   completed: true,
+  // }
+
+
   return (
     <>
     <Todo {...toDo}/>
